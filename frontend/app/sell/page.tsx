82afcc661eab17df6f775145b2e5fb49ae2f3e41
Emanuel <radu_emilian12@yahoo.com>
2026-08-02 16:59:58 +0300

    majore modifier Sellv2

--- frontend/app/sell/page.tsx
+++ frontend/app/sell/page.tsx
@@ -4,7 +4,7 @@ import { SellForm } from "@/app/components/Sell";
 
 export default function SellPage() {
     return (
-        <main className="min-h-screen bg-gradient-to-b from-gray-50 via-white to-gray-100">
+        <main className="min-h-screen bg-gray-50">
 
             <div className="mx-auto max-w-screen-2xl px-6 py-10">
 
