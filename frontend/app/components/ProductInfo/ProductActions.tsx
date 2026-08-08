7b73204b021c9d6eccf4297fbf654b652246ce51
Emanuel <radu_emilian12@yahoo.com>
2026-08-08 14:02:57 +0300

    majore modifier Sellv2.1

--- frontend/app/components/ProductInfo/ProductActions.tsx
+++ frontend/app/components/ProductInfo/ProductActions.tsx
@@ -9,7 +9,7 @@ import {
 
 import { notify } from "@/app/lib/notify";
 import Button from "@/app/components/ui/Button";
-import { useFavorite } from "@/app/hooks/useFavorite";
+import { useFavorite } from "@/app/hooks/useFavorites";
 
 interface Props {
     listingId: string;
