4789eb748fc9d8765d9d560638fb7c25cfe5ca4d
Emanuel <radu_emilian12@yahoo.com>
2026-08-16 18:46:09 +0300

    rework mobile

--- frontend/app/components/Header/LogoutButton.tsx
+++ frontend/app/components/Header/LogoutButton.tsx
@@ -7,7 +7,7 @@ export default function LogoutButton() {
 
   const handleLogout = () => {
     localStorage.removeItem("token");
-    router.replace("/login");
+    router.replace("/");
   };
 
   return (
